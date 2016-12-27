3dea6eaff2d4e3d4271e31c0caf12e5856cb203a
Ricky <rickycxj@gmail.com>
2016-12-27 11:26:34 +0800

    modern层代码重构

--- concerns/user_creator.rb
+++ concerns/user_creator.rb
@@ -2,36 +2,34 @@ module UserCreator
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def create_user_by_mobile(mobile)
-      create_params = require_attributes.merge({mobile: mobile})
-      user = User.new(create_params)
-      user.save
-      user
+    def create_by_mobile(mobile)
+      user_attrs = new_user_attributes({mobile: mobile})
+      User.create(user_attrs)
     end
 
-    def create_user_by_email(email, password)
-      require_attributes = require_attributes password
-      create_attributes = require_attributes.merge({email: email})
-      user = User.new(create_attributes)
-      user.save
-      user
+    def create_by_email(email, password)
+      user_attrs = new_user_attributes({email: email, password: password})
+      User.create(user_attrs)
     end
 
-    def require_attributes(password=nil)
-      password_salt = SecureRandom.hex(6).slice(0, 6)
-      password = password || ::Digest::MD5.hexdigest(SecureRandom.uuid)
-      salted_password = ::Digest::MD5.hexdigest(password + password_salt)
+    def new_user_attributes(user_attrs = {})
+      default_attrs = user_attrs.dup
+      password = default_attrs.delete(:password)
+      password ||= ::Digest::MD5.hexdigest(SecureRandom.uuid)
+      salt = SecureRandom.hex(6).slice(0, 6)
+      salted_password = ::Digest::MD5.hexdigest("#{password}#{salt}")
       user_name = User.unique_username
       nick_name = user_name
+
       {   user_uuid: SecureRandom.hex(16),
           user_name: user_name,
           nick_name: nick_name,
           password: salted_password,
-          password_salt: password_salt,
+          password_salt: salt,
           gender: 2,
           reg_date: Time.now,
           last_visit: Time.now
-      }
+      }.merge!(default_attrs)
     end
   end
 end
