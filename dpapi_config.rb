ecf264ee45068234c8b095167036f35300d1b325
Ricky <rickycxj@gmail.com>
2017-01-06 09:09:04 +0800

    rubocop检查
    Former-commit-id: 3d459f0dbef7dec9f28f0140914c49c3fd7fc4fd

--- dpapi_config.rb
+++ dpapi_config.rb
@@ -1,11 +1,10 @@
 class DpapiConfig
   cattr_accessor :config
-  self.config ||= YAML.load_file(Rails.root.join("config", "dpapi.yml"))[Rails.env]
-
+  self.config ||= YAML.load_file(Rails.root.join('config', 'dpapi.yml'))[Rails.env]
 
   class << self
     def domain_path
-      config["domain_path"].dup
+      config['domain_path'].dup
     end
   end
 end
