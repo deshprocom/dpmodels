15dcaf95369c5217c812bb127e901fe56ef94d02
Ricky <rickycxj@gmail.com>
2017-01-12 15:42:53 +0800

    修改密码验证规则 前端必须上传md5格式的密码才能通过验证

--- concerns/password_validator.rb
+++ concerns/password_validator.rb
@@ -3,8 +3,8 @@ module PasswordValidator
   # 有效的密码格式满足的条件
   # 1, 长度必须6 - 20位
   # 2, 必须是 数字+字母 或 数字 + 特殊字符 或 字母+特殊字符 或 数字 + 字母 + 特殊字符的组合
-  PWD_VALID_FORMAT_REGEX = /^(?![\d]+$)(?![a-zA-Z]+$)(?![^\da-zA-Z]+$).{6,20}$/
-
+  # 交给前端去判断 PWD_VALID_FORMAT_REGEX = /^(?![\d]+$)(?![a-zA-Z]+$)(?![^\da-zA-Z]+$).{6,20}$/
+  PWD_VALID_FORMAT_REGEX = /^[a-fA-F0-9]{32}$/
   extend ActiveSupport::Concern
 
   module ClassMethods
