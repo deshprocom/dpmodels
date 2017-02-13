fd2cdfb4a8d11ba9298638b23155a8dc12c4dc68
breeze-yang <breeze@deshpro.com>
2017-02-13 17:29:23 +0800

    增加ac_us017的factory api

--- race_ticket.rb
+++ race_ticket.rb
@@ -2,38 +2,15 @@
 # | Field      | Type         | Null | Key | Default | Extra          |
 # +------------+--------------+------+-----+---------+----------------+
 # | id         | int(11)      | NO   | PRI | NULL    | auto_increment |
-# | name       | varchar(256) | YES  |     | NULL    |                |
-# | seq_id     | bigint(20)   | NO   |     | 0       |                |
-# | logo       | varchar(256) | YES  |     | NULL    |                |
-# | prize      | int(11)      | NO   |     | 0       |                |
-# | location   | varchar(256) | YES  |     | NULL    |                |
-# | begin_date | date         | YES  |     | NULL    |                |
-# | end_date   | date         | YES  |     | NULL    |                |
-# | status     | int(11)      | NO   |     | 0       |                |
+# | race_id    | int(11)      | YES  | MUL | NULL    |                |
+# | name       | varchar(100) | YES  |     | NULL    |                |
+# | price      | int(11)      | YES  |     | 0       |                |
+# | status     | varchar(30)  | YES  |     | selling |                |
 # | created_at | datetime     | NO   |     | NULL    |                |
 # | updated_at | datetime     | NO   |     | NULL    |                |
 # +------------+--------------+------+-----+---------+----------------+
-class Race < ApplicationRecord
-  has_one :race_desc
-  has_many :race_follows
-  has_many :race_orders
 
-  # 近期赛事
-  scope :recent_races, -> { where('end_date >= ?', Time.zone.now.end_of_day).where.not(status: [2, 3]) }
+class RaceTicket < ApplicationRecord
+  belongs_to :race
 
-  # 排序
-  scope :order_race_list, -> { order(begin_date: :asc).order(end_date: :asc).order(created_at: :asc) }
-
-  # 获取指定条数的近期赛事 (5条)
-  def self.limit_recent_races(numbers = 5)
-    recent_races.limit(numbers).order_race_list
-  end
-
-  def followed?(user_id)
-    race_follows.exists?(user_id: user_id)
-  end
-
-  def ordered?(user_id)
-    race_orders.exists?(user_id: user_id)
-  end
 end
