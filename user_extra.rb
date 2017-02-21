475bb840499e41bdeb47810e3fa52aba49dda7cd
Ricky <rickycxj@gmail.com>
2017-02-21 15:36:55 +0800

    新增用户身份证图片上传 修改实名返回身份信息

--- user_extra.rb
+++ user_extra.rb
@@ -1,14 +1,35 @@
-# +-----------+--------------+------+-----+------------+----------------+
-# | Field     | Type         | Null | Key | Default    | Extra          |
-# +-----------+--------------+------+-----+------------+----------------+
-# | id        | int(11)      | NO   | PRI | NULL       | auto_increment |
-# | user_id   | int(11)      | YES  | MUL | NULL       |                |
-# | real_name | varchar(50)  | YES  |     | NULL       |                |
-# | cert_type | varchar(50)  | YES  |     | chinese_id |                |
-# | cert_no   | varchar(255) | YES  |     | NULL       |                |
-# | memo      | varchar(255) | YES  |     | NULL       |                |
-# +-----------+--------------+------+-----+------------+----------------+
-
+# +------------+--------------+------+-----+------------+----------------+
+# | Field      | Type         | Null | Key | Default    | Extra          |
+# +------------+--------------+------+-----+------------+----------------+
+# | id         | int(11)      | NO   | PRI | NULL       | auto_increment |
+# | user_id    | int(11)      | YES  | MUL | NULL       |                |
+# | real_name  | varchar(50)  | YES  |     | NULL       |                |
+# | cert_type  | varchar(50)  | YES  |     | chinese_id |                |
+# | cert_no    | varchar(255) | YES  |     | NULL       |                |
+# | memo       | varchar(255) | YES  |     | NULL       |                |
+# | image      | varchar(255) | YES  |     |            |                |
+# | image_md5  | varchar(32)  | NO   |     |            |                |
+# | status     | varchar(20)  | YES  |     | pending    |                |
+# | created_at | datetime     | NO   |     | NULL       |                |
+# | updated_at | datetime     | NO   |     | NULL       |                |
+# +------------+--------------+------+-----+------------+----------------+
 # 用户认证信息表
 class UserExtra < ApplicationRecord
+  mount_uploader :image, CardImageUploader
+
+  attr_accessor :image_path
+
+  def image=(value)
+    super
+
+    if image.file.present? &&
+       image.file.respond_to?(:path) &&
+       File.exist?(image.file.path)
+      self.image_md5 = Digest::MD5.file(image.file.path).hexdigest
+    end
+  end
+
+  def image_path
+    DpapiConfig.domain_path.to_s + image.to_s unless image.blank?
+  end
 end
