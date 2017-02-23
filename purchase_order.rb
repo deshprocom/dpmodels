4b13733aaab00329698b27e5c34bdb75121ff636
breeze-yang <breeze@deshpro.com>
2017-02-23 17:17:12 +0800

    为订单创建增加异常处理

--- purchase_order.rb
+++ purchase_order.rb
@@ -1,3 +1,24 @@
+=begin
++----------------+--------------+------+-----+----------+----------------+
+| Field          | Type         | Null | Key | Default  | Extra          |
++----------------+--------------+------+-----+----------+----------------+
+| id             | int(11)      | NO   | PRI | NULL     | auto_increment |
+| user_id        | int(11)      | YES  | MUL | NULL     |                |
+| ticket_id      | int(11)      | YES  | MUL | NULL     |                |
+| race_id        | int(11)      | YES  | MUL | NULL     |                |
+| ticket_type    | varchar(30)  | YES  |     | e_ticket |                |
+| email          | varchar(255) | YES  |     | NULL     |                |
+| address        | varchar(255) | YES  |     | NULL     |                |
+| consignee      | varchar(50)  | YES  |     | NULL     |                |
+| mobile         | varchar(50)  | YES  |     | NULL     |                |
+| order_number   | varchar(30)  | YES  | UNI | NULL     |                |
+| price          | int(11)      | NO   |     | NULL     |                |
+| original_price | int(11)      | NO   |     | NULL     |                |
+| status         | varchar(30)  | YES  | MUL | unpaid   |                |
+| created_at     | datetime     | NO   |     | NULL     |                |
+| updated_at     | datetime     | NO   |     | NULL     |                |
++----------------+--------------+------+-----+----------+----------------+
+=end
 class PurchaseOrder < ApplicationRecord
   include NumberGenerator
 
@@ -6,7 +27,8 @@ class PurchaseOrder < ApplicationRecord
   belongs_to :ticket
   has_one :snapshot, class_name: OrderSnapshot
 
-  validates_presence_of :user, :race, :ticket_type, :ticket, :order_number, :price, :original_price
+  validates :race, :order_number,
+            presence: true
 
   after_initialize do
     self.order_number ||= self.class.unique_number
--- ticket.rb
+++ ticket.rb
@@ -1,3 +1,21 @@
+=begin
++-----------------+--------------+------+-----+------------+----------------+
+| Field           | Type         | Null | Key | Default    | Extra          |
++-----------------+--------------+------+-----+------------+----------------+
+| id              | int(11)      | NO   | PRI | NULL       | auto_increment |
+| user_id         | int(11)      | YES  | MUL | NULL       |                |
+| race_id         | int(11)      | YES  | MUL | NULL       |                |
+| ticket_infos_id | int(11)      | YES  | MUL | NULL       |                |
+| cert_type       | varchar(50)  | YES  |     | chinese_id |                |
+| cert_no         | varchar(255) | YES  |     | NULL       |                |
+| ticket_type     | varchar(30)  | YES  |     | e_ticket   |                |
+| status          | varchar(30)  | YES  |     | unpaid     |                |
+| memo            | varchar(255) | YES  |     | NULL       |                |
+| created_at      | datetime     | NO   |     | NULL       |                |
+| updated_at      | datetime     | NO   |     | NULL       |                |
++-----------------+--------------+------+-----+------------+----------------+
+=end
+
 class Ticket < ApplicationRecord
   belongs_to :race
   belongs_to :user
