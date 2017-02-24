381e1e7119495eb09c65f9bb0372b3b7144f8e02
breeze-yang <breeze@deshpro.com>
2017-02-24 15:55:18 +0800

    增加取消订单后，可继续购票的rspec

--- ticket.rb
+++ ticket.rb
@@ -1,23 +1,26 @@
 =begin
-+-----------------+--------------+------+-----+------------+----------------+
-| Field           | Type         | Null | Key | Default    | Extra          |
-+-----------------+--------------+------+-----+------------+----------------+
-| id              | int(11)      | NO   | PRI | NULL       | auto_increment |
-| user_id         | int(11)      | YES  | MUL | NULL       |                |
-| race_id         | int(11)      | YES  | MUL | NULL       |                |
-| ticket_infos_id | int(11)      | YES  | MUL | NULL       |                |
-| ticket_type     | varchar(30)  | YES  |     | e_ticket   |                |
-| memo            | varchar(255) | YES  |     | NULL       |                |
-| created_at      | datetime     | NO   |     | NULL       |                |
-| updated_at      | datetime     | NO   |     | NULL       |                |
-+-----------------+--------------+------+-----+------------+----------------+
++-----------------+--------------+------+-----+----------+----------------+
+| Field           | Type         | Null | Key | Default  | Extra          |
++-----------------+--------------+------+-----+----------+----------------+
+| id              | int(11)      | NO   | PRI | NULL     | auto_increment |
+| user_id         | int(11)      | YES  | MUL | NULL     |                |
+| race_id         | int(11)      | YES  | MUL | NULL     |                |
+| ticket_infos_id | int(11)      | YES  | MUL | NULL     |                |
+| ticket_type     | varchar(30)  | YES  |     | e_ticket |                |
+| memo            | varchar(255) | YES  |     | NULL     |                |
+| canceled        | tinyint(1)   | YES  |     | 0        |                |
+| created_at      | datetime     | NO   |     | NULL     |                |
+| updated_at      | datetime     | NO   |     | NULL     |                |
++-----------------+--------------+------+-----+----------+----------------+
 =end
 
 class Ticket < ApplicationRecord
   belongs_to :race
   belongs_to :user
 
+  scope :valid, -> { where(canceled: false) }
+
   def self.again_buy?(user_id, race_id)
-    where(user_id: user_id).where(race_id: race_id).present?
+    where(user_id: user_id).where(race_id: race_id).valid.present?
   end
 end
