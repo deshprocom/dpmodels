790976988b15d6c0c70f7400a94523da8320f138
breeze-yang <breeze@deshpro.com>
2017-02-24 14:22:16 +0800

    重构tikcet的逻辑

--- ticket.rb
+++ ticket.rb
@@ -6,8 +6,6 @@
 | user_id         | int(11)      | YES  | MUL | NULL       |                |
 | race_id         | int(11)      | YES  | MUL | NULL       |                |
 | ticket_infos_id | int(11)      | YES  | MUL | NULL       |                |
-| cert_type       | varchar(50)  | YES  |     | chinese_id |                |
-| cert_no         | varchar(255) | YES  |     | NULL       |                |
 | ticket_type     | varchar(30)  | YES  |     | e_ticket   |                |
 | status          | varchar(30)  | YES  |     | unpaid     |                |
 | memo            | varchar(255) | YES  |     | NULL       |                |
