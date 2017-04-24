fbd3d3eda9320d7162575629eafcd07d4a5be640
Ricky <rickycxj@gmail.com>
2017-04-24 16:43:50 +0800

    修改info

--- info.rb
+++ info.rb
@@ -2,6 +2,9 @@ class Info < ApplicationRecord
   belongs_to :info_type
   default_scope { where(published: true) } unless ENV['CURRENT_PROJECT'] == 'dpcms'
 
+  scope :published, -> { where(published: true) }
+  scope :topped, -> { where(top: true) }
+
   def publish!
     update(published: true)
   end
