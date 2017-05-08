eab066fa2935392198767421987103e491acdf09
breeze-yang <breeze@deshpro.com>
2017-05-08 15:58:32 +0800

    修改race_blind

--- race_blind.rb
+++ race_blind.rb
@@ -2,7 +2,8 @@ class RaceBlind < ApplicationRecord
   belongs_to :race
 
   validates :level, numericality: { greater_than: 0 }
+  validates :content, presence: true, if: :blind_content?
 
-  enum blind_type: [:blind_struct, :content]
+  enum blind_type: [:blind_struct, :blind_content]
   scope :level_asc, -> { order(level: :asc).order(blind_type: :asc) }
 end
