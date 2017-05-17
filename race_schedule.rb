29cad5ff4287a684803e84ca2e597d952c3ec85f
Ricky <rickycxj@gmail.com>
2017-05-17 16:03:02 +0800

    修改schedule排序

--- race_schedule.rb
+++ race_schedule.rb
@@ -4,5 +4,6 @@ class RaceSchedule < ApplicationRecord
     self.begin_time ||= Time.current
   end
 
-  scope :default_order, -> { order(schedule: :asc) }
+  scope :default_order, -> { order(begin_time: :asc, schedule: :asc) }
+  scope :default_asc, -> { order(begin_time: :asc, schedule: :asc) }
 end
