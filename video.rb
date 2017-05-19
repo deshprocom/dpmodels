e5fbbaaabda720680995a57343aa74d20c68d275
Ricky <rickycxj@gmail.com>
2017-05-19 16:23:46 +0800

    视频相关的模型

--- video.rb
+++ video.rb
@@ -1,2 +1,28 @@
 class Video < ApplicationRecord
+  belongs_to :video_type
+
+  before_save do
+    self.description = ActionController::Base.helpers.strip_tags(description)
+  end
+
+  default_scope { where(published: true) } unless ENV['CURRENT_PROJECT'] == 'dpcms'
+
+  scope :published, -> { where(published: true) }
+  scope :topped, -> { where(top: true) }
+
+  def publish!
+    update(published: true)
+  end
+
+  def unpublish!
+    update(published: false)
+  end
+
+  def top!
+    update(top: true)
+  end
+
+  def untop!
+    update(top: false)
+  end
 end
