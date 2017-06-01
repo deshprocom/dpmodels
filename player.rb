494a0400174f4a4c72c177b2c09d877bd83b7d6b
Ricky <rickycxj@gmail.com>
2017-06-01 15:53:53 +0800

    player

--- player.rb
+++ player.rb
@@ -21,7 +21,7 @@
 class Player < ApplicationRecord
   mount_uploader :avatar, PlayerUploader
   has_many :race_ranks
-  attr_accessor :avatar_path
+  attr_accessor :avatar_thumb
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
   after_update :crop_avatar
 
