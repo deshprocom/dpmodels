4cbad0ff98b8e15e4806860ad0b114577aa1a26a
Ricky <rickycxj@gmail.com>
2017-06-27 12:15:47 +0800

    info_type

--- info_type.rb
+++ info_type.rb
@@ -1,6 +1,7 @@
 class InfoType < ApplicationRecord
   has_many :infos, dependent: :destroy
   has_one  :eng_info_type
+  accepts_nested_attributes_for :eng_info_type
 
   default_scope { where(published: true) } unless ENV['CURRENT_PROJECT'] == 'dpcms'
 
