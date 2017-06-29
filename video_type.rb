cb9c73fb3d0452ece8c313fc1ec93dd60727caf7
Ricky <rickycxj@gmail.com>
2017-06-29 10:53:44 +0800

    视频

--- video_type.rb
+++ video_type.rb
@@ -1,5 +1,8 @@
 class VideoType < ApplicationRecord
   has_many :videos, dependent: :destroy
+  has_one  :video_type_en, dependent: :destroy
+  accepts_nested_attributes_for :video_type_en, allow_destroy: true
+
   default_scope { where(published: true) } unless ENV['CURRENT_PROJECT'] == 'dpcms'
 
   def publish!
