3313791873300cbe40219ed1c966e2ae91b424fa
Ricky <rickycxj@gmail.com>
2017-07-17 17:21:14 +0800

    rubocop

--- info_en.rb
+++ info_en.rb
@@ -4,6 +4,6 @@ class InfoEn < ApplicationRecord
 
   before_save do
     diff_attrs = %w(title source description)
-    assign_attributes info.reload.attributes.reject {|k| attributes[k].present? && k.in?(diff_attrs) }
+    assign_attributes info.reload.attributes.reject { |k| attributes[k].present? && k.in?(diff_attrs) }
   end
 end
