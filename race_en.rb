7aba159ae05bc2fc32ec2feabf03ed368039f6e9
breeze-yang <breeze@deshpro.com>
2017-07-18 10:47:12 +0800

    修改race

--- race_en.rb
+++ race_en.rb
@@ -1,3 +1,10 @@
 class RaceEn < ApplicationRecord
   belongs_to :race, foreign_key: :id
+
+  before_save do
+    diff_attrs = %w(name prize location ticket_price blind)
+    assign_attributes race.reload.attributes.reject {|k| attributes[k].present? && k.in?(diff_attrs) }
+  end
+
+  enum status: [:unbegin, :go_ahead, :ended, :closed]
 end
