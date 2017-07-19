38a2ae0bed6032c5892900ddd4029f41dd5b2fc1
breeze-yang <breeze@deshpro.com>
2017-07-19 18:44:37 +0800

    修改race, ticket中英文

--- ticket_en.rb
+++ ticket_en.rb
@@ -3,7 +3,7 @@ class TicketEn < ApplicationRecord
 
   before_save do
     self.description = ActionController::Base.helpers.strip_tags(description)
-    diff_attrs = %w(title price original_price description)
+    diff_attrs = %w(title price original_price description updated_at)
     assign_attributes ticket.reload.attributes.reject { |k| attributes[k].present? && k.in?(diff_attrs) }
   end
 end
