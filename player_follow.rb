6711a6bdbc36a91ab86938a283d40548597ef880
breeze-yang <breeze@deshpro.com>
2017-07-27 17:04:06 +0800

    增加followed?

--- player_follow.rb
+++ player_follow.rb
@@ -1,4 +1,8 @@
 class PlayerFollow < ApplicationRecord
   belongs_to :user
   belongs_to :player
+
+  def self.followed?(user_id, player_id)
+    where(user_id: user_id).where(player_id: player_id).exists?
+  end
 end
