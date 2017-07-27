e11cc604fb1f7998fa8a21cb9e02586f78418cf9
breeze-yang <breeze@deshpro.com>
2017-07-27 16:22:06 +0800

    为user增加 followed_players

--- user.rb
+++ user.rb
@@ -40,6 +40,7 @@ class User < ApplicationRecord
   has_many :shipping_addresses, -> { order(default: :desc) }
   has_many :account_change_stats
   has_many :notifications
+  has_many :followed_players, -> { order(id: :desc) }, class_name: PlayerFollow
   accepts_nested_attributes_for :user_extra, allow_destroy: true
 
   enum status: { basic: 'basic', banned: 'banned' }
