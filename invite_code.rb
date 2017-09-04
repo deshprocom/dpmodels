24c933e8fef759afdce15b66c59d649283c356f1
Ricky <rickycxj@gmail.com>
2017-09-04 17:33:57 +0800

    邀请码

---
+++ invite_code.rb
@@ -0,0 +1,13 @@
+class InviteCode < ApplicationRecord
+  # 生成邀请码，唯一性
+  before_create :generate_code
+
+  protected
+
+  def generate_code
+    self.code = loop do
+      random_code = ('A'..'Z').to_a.sample(4).join
+      break random_code unless InviteCode.exists?(code: random_code)
+    end
+  end
+end
