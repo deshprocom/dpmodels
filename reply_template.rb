3551224ef5a4abcb125fb3442d27e864a5439394
Ricky <rickycxj@gmail.com>
2017-09-25 14:46:27 +0800

    回复模版

--- reply_template.rb
+++ reply_template.rb
@@ -1,4 +1,5 @@
 class ReplyTemplate < ApplicationRecord
   validates :source, :content, presence: true
   validates :content, uniqueness: true
+  belongs_to :template_type, foreign_key: :type_id
 end
