e9aa6ff90cf2d020566ce72192f2aac35fc9c42d
breeze-yang <breeze@deshpro.com>
2017-10-10 12:21:21 +0800

    完善headline

--- headline.rb
+++ headline.rb
@@ -7,6 +7,9 @@ class Headline < ApplicationRecord
   validates :source_type, presence: true
   validates :source_id, presence: true
 
+  scope :default_order, -> { order(position: :asc) }
+  scope :published, -> { where(published: true) }
+
   def source_title
     return if source.nil?
 
