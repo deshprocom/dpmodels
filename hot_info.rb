de2081a903be5162e4a52c1240bbb5f1da5337ce
breeze-yang <breeze@deshpro.com>
2017-10-11 11:59:04 +0800

    完善hot_info

--- hot_info.rb
+++ hot_info.rb
@@ -3,6 +3,7 @@ class HotInfo < ApplicationRecord
   attr_accessor :source_title
   validates :source_type, presence: true
   validates :source_id, presence: true
+  scope :default_order, -> { order(position: :asc) }
 
   def source_title
     return if source.nil?
