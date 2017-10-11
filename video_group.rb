fbdac411fa6419ee1737c8ead3486d5f3b0775e6
Ricky <rickycxj@gmail.com>
2017-10-11 17:28:00 +0800

    主视频

--- video_group.rb
+++ video_group.rb
@@ -1,3 +1,5 @@
 class VideoGroup < ApplicationRecord
   has_many :videos
+  has_one :video_group_en, foreign_key: 'id', dependent: :destroy
+  accepts_nested_attributes_for :video_group_en, update_only: true
 end
--- video_group_en.rb
+++ video_group_en.rb
@@ -1,3 +1,4 @@
 class VideoGroupEn < ApplicationRecord
   has_many :video_ens
+  belongs_to :video_group, foreign_key: 'id'
 end
