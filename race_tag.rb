17fe2d626fb4c677feaa572835ced43f409539f0
Ricky <rickycxj@gmail.com>
2017-10-27 16:14:42 +0800

    为资讯和视频加上标签

--- race_tag.rb
+++ race_tag.rb
@@ -1,3 +1,6 @@
 class RaceTag < ApplicationRecord
   has_many :race_tag_maps
+  has_one  :race_tag_en, foreign_key: 'id', dependent: :destroy
+  accepts_nested_attributes_for :race_tag_en, update_only: true
+  after_update { race_tag_en&.save }
 end
