5b14e8f83e6a5b7a2d5375231ea0c7a08bd8dffd
breeze-yang <breeze@deshpro.com>
2017-11-06 18:05:51 +0800

    为商品支持图片功能

---
+++ product_image.rb
@@ -0,0 +1,11 @@
+class ProductImage < ApplicationRecord
+  mount_uploader :filename, ProductImageUploader
+
+  belongs_to :viewable, polymorphic: true
+
+  def preview
+    return '' if filename.url.nil?
+
+    filename.url(:sm)
+  end
+end
