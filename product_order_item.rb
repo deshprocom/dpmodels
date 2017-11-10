2117a0071f3e67b6897340433bd8932b8f9c778d
Ricky <rickycxj@gmail.com>
2017-11-10 16:44:08 +0800

    修复rubocop

--- product_order_item.rb
+++ product_order_item.rb
@@ -4,6 +4,6 @@ class ProductOrderItem < ApplicationRecord
 
   after_create do
     total = number * price
-    self.product_order.increment!(:total_product_price, total)
+    product_order.increment!(:total_product_price, total)
   end
 end
