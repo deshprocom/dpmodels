0bf481ff4a0c669f8bb172e81070bf805cc3240c
Ricky <rickycxj@gmail.com>
2017-11-11 18:08:18 +0800

    生成item的时候自动生成数据

--- product_order_item.rb
+++ product_order_item.rb
@@ -2,8 +2,21 @@ class ProductOrderItem < ApplicationRecord
   belongs_to :product_order
   belongs_to :variant
 
+  before_create :generate_snapshot
+
   after_create do
     total = number * price
     self.product_order.increment!(:total_product_price, total)
+  end
+
+
+  def generate_snapshot
+    self.original_price = variant.original_price
+    self.price = variant.price
+    option_values = variant&.option_values
+    return unless option_values
+    self.sku_value = option_values.collect do |option|
+      { option.option_type.name => option.name }
+    end
   end
 end
