cf6510ca81ba0e4fef41a9b54e050eedc15db439
Ricky <rickycxj@gmail.com>
2017-11-14 15:46:21 +0800

    发货部分

--- express_code.rb
+++ express_code.rb
@@ -1,2 +1,5 @@
 class ExpressCode < ApplicationRecord
+  has_many :product_shipments
+
+  default_scope { where(published: true) } unless ENV['CURRENT_PROJECT'] == 'dpcms'
 end
--- product_shipment.rb
+++ product_shipment.rb
@@ -1,2 +1,4 @@
 class ProductShipment < ApplicationRecord
+  belongs_to :express_code, optional: true
+  belongs_to :product_order
 end
