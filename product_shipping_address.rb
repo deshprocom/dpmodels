b144a813752268b3275ab95231d9fdf630fd17a0
breeze-yang <breeze@deshpro.com>
2017-11-16 17:58:40 +0800

    修改product_shipping_address.rb

--- product_shipping_address.rb
+++ product_shipping_address.rb
@@ -1,5 +1,5 @@
 class ProductShippingAddress < ApplicationRecord
-  belongs_to :product_order, optional: true
+  belongs_to :product_order
 
   def full_address
     province.to_s + city.to_s + area.to_s + address.to_s
