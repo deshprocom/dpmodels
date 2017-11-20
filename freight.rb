19a897881749799d24e3e80d2a415036000cee6b
Ricky <rickycxj@gmail.com>
2017-11-20 14:42:23 +0800

    调整运费计算方式

--- freight.rb
+++ freight.rb
@@ -14,15 +14,31 @@ class Freight < ApplicationRecord
     update(default: false)
   end
 
-  def product_freight(condition, destination)
-    obj = fre_special_provinces.where(province_name: destination)
-    return obj.first.fre_special.calculate(condition) if obj.exists?
-    calculate(condition)
+  # 获取商品的运费
+  def product_freight(destination, options = {})
+    params = {
+      weight: options.delete(:weight) || 1,
+      number: options.delete(:number) || 1,
+      volume: options.delete(:volume) || 1
+    }
+    condition = calculate_condition(params)
+    calculate(find_province_freight(destination), condition)
   end
 
-  def calculate(condition)
-    diff = condition - first_cond
-    return first_price if diff <= 0
-    first_price + (diff / add_cond).ceil * add_price
+  def calculate_condition(params)
+    case freight_type
+    when 'number'
+      params[:number]
+    when 'weight'
+      params[:number] * params[:weight]
+    when 'volume'
+      params[:number] * params[:volume]
+    end
+  end
+
+  def calculate(spe_freight, condition)
+    diff = condition - spe_freight.first_cond
+    return spe_freight.first_price if diff <= 0
+    spe_freight.first_price + (diff / spe_freight.add_cond).ceil * spe_freight.add_price
   end
 end
