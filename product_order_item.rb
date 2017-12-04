4f8e1fe0116175a68820024e0a59cc55d0d06196
Ricky <rickycxj@gmail.com>
2017-12-04 19:08:00 +0800

    更新退款接口

--- product_order_item.rb
+++ product_order_item.rb
@@ -1,7 +1,7 @@
 class ProductOrderItem < ApplicationRecord
   belongs_to :product_order
   belongs_to :variant
-  has_one :product_refund_detail, dependent: :destroy
+  has_many :product_refund_details, dependent: :destroy
 
   before_save :syn_variant
   serialize :sku_value, JSON
