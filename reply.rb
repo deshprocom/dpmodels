2549252748d4afbf88c8e6dfc70649e226e6a55d
Ricky <rickycxj@gmail.com>
2017-12-19 12:19:34 +0800

    个人中心

--- reply.rb
+++ reply.rb
@@ -3,7 +3,7 @@ class Reply < ApplicationRecord
   belongs_to :comment
   belongs_to :user
   has_many :dynamics, as: :typological, dependent: :destroy
-  has_many :subreplies, class_name: 'Reply', foreign_key: 'reply_id', dependent: :destroy
+  has_many :replies, class_name: 'Reply', foreign_key: 'reply_id', dependent: :destroy
   belongs_to :reply, optional: true
   include Typologicalable
 end
