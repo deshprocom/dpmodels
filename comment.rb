13571a4da067e6b71dfc2e51734935ac0e1b7fd3
Ricky <rickycxj@gmail.com>
2017-12-20 15:14:35 +0800

    评论管理界面

--- comment.rb
+++ comment.rb
@@ -4,4 +4,12 @@ class Comment < ApplicationRecord
   belongs_to :user
   has_many :dynamics, as: :typological, dependent: :destroy
   include Typologicalable
+  include Recommendable
+
+  default_scope { where(deleted: false) }
+
+  def admin_delete(reason = '')
+    update(deleted_reason: reason, deleted_at: Time.zone.now, deleted: true)
+    Dynamic.create(user: User.official, typological: self, option_type: 'delete')
+  end
 end
--- reply.rb
+++ reply.rb
@@ -6,4 +6,11 @@ class Reply < ApplicationRecord
   has_many :replies, class_name: 'Reply', foreign_key: 'reply_id', dependent: :destroy
   belongs_to :reply, optional: true
   include Typologicalable
+
+  default_scope { where(deleted: false) }
+
+  def admin_delete(reason = '')
+    update(deleted_reason: reason, deleted_at: Time.zone.now, deleted: true)
+    Dynamic.create(user: User.official, typological: self, option_type: 'delete')
+  end
 end
