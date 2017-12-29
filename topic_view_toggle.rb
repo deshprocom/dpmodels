a7e3a7c3075c719c5bbfdb2dd231ed0e4913e852
Ricky <rickycxj@gmail.com>
2017-12-29 17:09:04 +0800

    添加浏览量增量开关

--- topic_view_toggle.rb
+++ topic_view_toggle.rb
@@ -3,20 +3,19 @@ class TopicViewToggle < ApplicationRecord
 
   # 只更新打开并且间隔时间在规则之内并且打开两个月之内的
   def self.toggle_on
-    where('toggle_status', true) &&
-        where('begin_time > ?', 60.days.ago)
+    where('toggle_status', true) && where('begin_time > ?', 60.days.ago)
   end
 
   # 得到距离今天是第几天
   def period
-    (Date.today - begin_time.to_date).to_i
+    (Time.zone.today - begin_time.to_date).to_i
   end
 
   def current_rule
     current_period = period + 1
     last_rule = TopicViewRule.where('hot': hot).order('day': :desc).last
     return last_rule if current_period > last_rule.day
-    rule = TopicViewRule.where('day': current_period).where('hot': hot).first
+    rule = TopicViewRule.where('day': current_period).find_by('hot': hot)
     rule.blank? ? last_rule : rule
   end
 end
