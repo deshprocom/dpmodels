56b9516697cb6577b18506e264a93c41ceda1dab
Ricky <rickycxj@gmail.com>
2018-01-10 16:09:21 +0800

    众筹管理

--- crowdfunding.rb
+++ crowdfunding.rb
@@ -1,4 +1,18 @@
 class Crowdfunding < ApplicationRecord
+  mount_uploader :master_image, CrowdfundingUploader
+
   belongs_to :race
   has_many :players
+
+  after_initialize do
+    self.expire_date ||= Date.current
+    self.publish_date ||= Date.current
+    self.award_date ||= Date.current
+  end
+
+  def preview_image
+    return '' if master_image.url.nil?
+
+    master_image.url(:sm)
+  end
 end
