476a2bd236f88a34928f3692d3d60b549706fae5
Ricky <rickycxj@gmail.com>
2018-01-26 18:26:08 +0800

    扑客币

--- crowdfunding_rank.rb
+++ crowdfunding_rank.rb
@@ -16,8 +16,10 @@ class CrowdfundingRank < ApplicationRecord
   after_save do
     if !awarded && !finaled
       crowdfunding_player.crowdfunding_orders.each(&:failed!)
+      crowdfunding_player.failed!
     else
       crowdfunding_player.crowdfunding_orders.each(&:success!)
+      crowdfunding_player.success!
     end
   end
 
--- poker_coin.rb
+++ poker_coin.rb
@@ -5,4 +5,9 @@ class PokerCoin < ApplicationRecord
   after_create do
     user.increase_poker_coins(number)
   end
+
+  # resource 对应的是PokerCoin所关联的typeable
+  def self.total_coin_of_the_type(resource, user)
+    where(typeable: resource).where(user: user).sum(:number)
+  end
 end
