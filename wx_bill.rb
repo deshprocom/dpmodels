cd9f7af62d53a1d4da70ed1c310ee790370bdc50
Ricky <rickycxj@gmail.com>
2018-02-02 14:35:31 +0800

    修复rubocop

--- wx_bill.rb
+++ wx_bill.rb
@@ -1,8 +1,8 @@
 class WxBill < ApplicationRecord
   belongs_to :order, class_name: 'PurchaseOrder',
-             primary_key: :order_number,
-             foreign_key: :out_trade_no,
-             optional: true
+                     primary_key: :order_number,
+                     foreign_key: :out_trade_no,
+                     optional: true
 
   def self.success_bills
     where(result_code: 'SUCCESS').where(return_code: 'SUCCESS')
