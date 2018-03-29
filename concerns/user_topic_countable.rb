34e9f43ac1240544eb91f82b627871fa12f4c9c6
Ricky <rickycxj@gmail.com>
2018-03-29 17:38:10 +0800

    个人动态 说说点赞功能

--- concerns/user_topic_countable.rb
+++ concerns/user_topic_countable.rb
@@ -3,4 +3,16 @@ module UserTopicCountable
   included do
     after_create :create_counter
   end
+
+  def increase_page_views
+    counter.increment!(:page_views)
+  end
+
+  def increase_likes
+    counter.increment!(:likes)
+  end
+
+  def decrease_likes
+    counter.decrement!(:likes)
+  end
 end
--- user_topic.rb
+++ user_topic.rb
@@ -3,12 +3,18 @@ class UserTopic < ApplicationRecord
   include UserTopicCountable
   has_many :topic_images, class_name: 'UserTopicImage'
   has_one :counter, class_name: 'UserTopicCounter', dependent: :destroy
+  has_many :comments, as: :topic, dependent: :destroy
 
   scope :undeleted, -> { where(deleted: false) }
   scope :sorted, -> { order(created_at: :desc) }
   scope :recommended, -> { where(recommended: true) }
+  scope :order_show, -> { order(recommended: :desc).order(created_at: :desc) }
 
   def publish!
     update(published: true, published_time: Time.zone.now)
+  end
+
+  def total_likes
+    counter.likes
   end
 end
