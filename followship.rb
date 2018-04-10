a06f58cea2add333dd69dfe92683d1c3a4d85510
shootingfly <790174750@qq.com>
2018-04-10 15:23:29 +0800

    分拆关注及粉丝列表

--- followship.rb
+++ followship.rb
@@ -2,18 +2,26 @@ class Followship < ApplicationRecord
   belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
   belongs_to :following, class_name: 'User', foreign_key: 'following_id'
 
-  validate :check_uniq
-  def check_uniq
+  validate on: :create do
     errors.add(:base, '已存在') if Followship.where(follower: follower_id, following: following_id).exists?
   end
 
-  after_create do
-    UserCounter.find_by!(user_id: follower_id).increment!(:following_count)
-    UserCounter.find_by!(user_id: following_id).increment!(:follower_count)
+  def self.follow(user, followed_user)
+    result = find_by(follower: followed_user, following: user)
+    followship = create(follower: user, following: followed_user, is_following: result.present?)
+    return if followship.errors.present?
+    result.update!(is_following: true) if result.present?
+
+    user.counter.increment!(:following_count)
+    followed_user.counter.increment!(:follower_count)
   end
 
-  before_destroy do
-    UserCounter.find_by!(user_id: follower_id).decrement!(:following_count)
-    UserCounter.find_by!(user_id: following_id).decrement!(:follower_count)
+  def self.unfollow(user, followed_user)
+    find_by!(follower: user, following: followed_user).destroy
+    result = find_by(follower: followed_user, following: user)
+    result.update!(is_following: false) if result.present?
+
+    user.counter.decrement!(:following_count)
+    followed_user.counter.decrement!(:follower_count)
   end
 end
